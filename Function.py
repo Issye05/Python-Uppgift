42a8b91c05643b1789354dff46733120a31f63ec
Isak Edholm <153501059+Issye05@users.noreply.github.com>
2025-10-19 13:43:55 +0200

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,30 @@
+def Word_Frequency(Link, Unique_Word):
+    Word_Count = {}
+    Words_Appearing_Only_Once = 0
+    Unique_Word_Count = 0
+    
+    with open(Link, "r", encoding="utf-8") as Book:
+
+        for Line in Book:
+            Words = Line.split()
+
+            for Word in Words:
+
+                Word = Word.lower()
+                Unique_Word = Unique_Word.lower()
+
+                if Word == Unique_Word:
+                    Unique_Word_Count += 1
+
+                if Word in Word_Count:
+                    Word_Count[Word] += 1
+                else:
+                    Word_Count[Word] = 1
+                    Words_Appearing_Only_Once += 1
+
+
+
+    Word_Count = sorted(Word_Count.items(), key=lambda x: x[1], reverse=True)
+    Word_Count = Word_Count[:10]
+
+    return Word_Count, Words_Appearing_Only_Once, Unique_Word_Count
